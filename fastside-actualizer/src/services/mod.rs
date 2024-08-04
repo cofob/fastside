b9fda6e4fbd314b47d15c49caad3a5b5bcbf1a5a
cofob <cofob@riseup.net>
2024-08-05 03:05:51 +0400

    Check instances in parallel

--- fastside-actualizer/src/services/mod.rs
+++ fastside-actualizer/src/services/mod.rs
@@ -6,7 +6,7 @@ use crate::types::ServiceUpdater;
 pub use default::DefaultInstanceChecker;
 
 /// Get a service updater by name.
-pub fn get_service_updater(name: &str) -> Option<Box<dyn ServiceUpdater>> {
+pub fn get_service_updater(name: &str) -> Option<Box<dyn ServiceUpdater + Send>> {
     match name {
         "searx" => Some(Box::new(searx::SearxUpdater::new())),
         _ => None,
@@ -14,7 +14,7 @@ pub fn get_service_updater(name: &str) -> Option<Box<dyn ServiceUpdater>> {
 }
 
 /// Get an instance checker by name.
-pub fn get_instance_checker(name: &str) -> Box<dyn crate::types::InstanceChecker> {
+pub fn get_instance_checker(name: &str) -> Box<dyn crate::types::InstanceChecker + Send> {
     match name {
         "searx" => Box::new(searx::SearxUpdater::new()),
         _ => Box::new(DefaultInstanceChecker::new()),
